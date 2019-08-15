786e70e9eb24e37a5aeedd14e8cc335a039a1881
Aniket Mazumder <mazumderaniket287@gmail.com>
2019-08-15 23:05:59 +0200

    Added new Project UART.X

---
+++ UARTSetup.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=UARTSetup.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/UARTSetup.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=uartsetup.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/uartsetup.x.tar
